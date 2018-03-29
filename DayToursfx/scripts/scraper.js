313a7fe9c628423b08c0f5bdb2b3a8db1d4fe9af
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-03-29 19:02:03 +0000

    drög að schema

--- DayToursfx/scripts/scraper.js
+++ DayToursfx/scripts/scraper.js
@@ -1,19 +1,76 @@
-const fs = require('fs');
+require('isomorphic-fetch');
+const cheerio = require('cheerio');
+
 const util = require('util');
-const writeFileAsync = util.promisify(fs.writeFile);
 
 const output = 'scripts/test.txt';
 const encoding = 'utf8';
+const baseurl = 'https://www.igtours.is';
 
-async function write(filepath, content) {
-  return writeFileAsync(filepath, content, { encoding });
+async function fetchBase() {
+	const response = await fetch(`${baseurl}/en/tours`);
+	const html = await response.text();
+
+	const $ = cheerio.load(html);
+
+	return $
+}
+
+function getLinks($) {
+	const li = $('li h2 a');
+
+	const links = [];
+
+	li.each((i, link) => {
+		const href = $(link).attr('href');
+		links.push(href);
+	});	
+
+	return links
+}
+
+function maketour($) {
+	
+	const basicInfo = $('#sidebar .sidebar .box');
+	console.log(basicInfo.text());
+	
+	
+	
+
+/* 	basicInfo.each((i, info) => {
+		console.log(info);
+		console.log('t');
+		
+	}); */
+}
+
+async function fetchTours(links) {
+	console.log(links);
+	
+	const tours = [];
+
+	links.forEach(async (url) => {
+		const response = await fetch(`${baseurl}${url}`);
+		const html = await response.text();
+		
+		const $ = cheerio.load(html);
+		
+
+		tours.push(maketour($));
+
+	});
 }
 
 async function main() {
 	console.log('start');
 	
-  await write(output, "plís virkaðu");
-  console.log(`Done writing ${output}`)
+	const $ = await fetchBase();
+	
+	links = getLinks($);
+	fetchTours(links);
+	
+	console.log('finito');
+	
 }
 
 main();
---
+++ schema.sql
@@ -0,0 +1,26 @@
+create table tours ( 
+    id SERIAL PRIMARY KEY,
+    title varchar(64) UNIQUE NOT NULL,
+    location varchar(64) NOT NULL,
+    duration varchar(64) NOT NULL,
+    difficulty varchar(64),
+    iternirary test
+);
+
+create table reviews(
+    id SERIAL PRIMARY KEY,
+    tourId references tours(id),
+    name varchar(64) NOT NULL,
+    email varchar(84),
+    date Date /** ATH kanski varchar bara?
+    review text
+);
+
+create table bookings(
+    id SERIAL PRIMARY KEY,
+    Purchaser_name varchar(64),
+    Purchaser_email varchar(84),
+    tourId references tours(id),
+    seats INTEGER
+);
+
