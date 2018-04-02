7cbd3e9892bc279ea1aa0dc4fca474b2db064b34
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-04-02 19:50:43 +0000

    update

--- DayToursfx/test/application/BookTest.java
+++ DayToursfx/test/application/BookTest.java
@@ -2,13 +2,33 @@ package application;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class BookTest {
+	
+	private Book bookingClass;
+	private int id;
+	
+	@Before
+	public void setUp() {
+		bookingClass = new Book();
+		id = 1;
+	}
+	
+	@After
+	public void tearDown() {
+		bookingClass = null;
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testName() {
+		assertNull(bookingClass.makeBooking(id, "", "valid@email.com", 5));
+		assertNull(bookingClass.makeBooking(id, 1, "valid@email.com", 5));
+		assertNull(bookingClass.makeBooking(id, "e", "valid@email.com", 5));
+		assertNull(bookingClass.makeBooking(id, false, "valid@email.com", 5));
+		assertNotNull(bookingClass.makeBooking(id, "Name", "valid@email.com", 5));
 	}
 
 }
