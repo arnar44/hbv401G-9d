45dbb819db7e2e99dade7a920e3b7cc29224682a
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-04-03 22:14:40 +0000

    baeta vid smid

--- DayToursfx/src/model/Tour.java
+++ DayToursfx/src/model/Tour.java
@@ -4,8 +4,12 @@ public class Tour {
 	private int id;
 	private String title, location, duration, difficulty, itinirary;
 	
-	public Tour() {
-		this.id = -1;
+	public Tour(String title, String location, String duration, String difficulty, String itinirary, int id) {
+		this.id = id;
+		this.location = location;
+		this.duration = duration;
+		this.difficulty = difficulty;
+		this.itinirary = itinirary;
 	}
 
 	public int getId() {
@@ -20,41 +24,19 @@ public class Tour {
 		return title;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
 	public String getLocation() {
 		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
 	}
 
 	public String getDuration() {
 		return duration;
 	}
 
-	public void setDuration(String duration) {
-		this.duration = duration;
-	}
-
 	public String getDifficulty() {
 		return difficulty;
-	}
-
-	public void setDifficulty(String difficulty) {
-		this.difficulty = difficulty;
 	}
 
 	public String getItinirary() {
 		return itinirary;
 	}
-
-	public void setItinirary(String itinirary) {
-		this.itinirary = itinirary;
-	}
-	
-
 }
