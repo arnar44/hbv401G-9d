116c92011f5552ebddff955d21e3f79365a5299e
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-04-03 02:30:29 +0000

    test eining

--- DayToursfx/test/application/BookTest.java
+++ DayToursfx/test/application/BookTest.java
@@ -27,7 +27,26 @@ class BookTest {
 		assertNull(bookingClass.makeBooking(id, "", "valid@email.com", 5));
 		assertNull(bookingClass.makeBooking(id, 1, "valid@email.com", 5));
 		assertNull(bookingClass.makeBooking(id, "e", "valid@email.com", 5));
-		assertNull(bookingClass.makeBooking(id, false, "valid@email.com", 5));
+		assertNull(bookingClass.makeBooking(id, null, "valid@email.com", 5));
+		assertNotNull(bookingClass.makeBooking(id, "Name", "valid@email.com", 5));
+	}
+	
+	@Test
+	public void testEmail() {
+		assertNull(bookingClass.makeBooking(id, "Name", "unvalidemail.com", 5));
+		assertNull(bookingClass.makeBooking(id, "Name", "unvalid@emailcom", 5));
+		assertNull(bookingClass.makeBooking(id, "Name", 12345, 5));
+		assertNull(bookingClass.makeBooking(id, "Name", null, 5));
+		assertNotNull(bookingClass.makeBooking(id, "Name", "valid@email.com", 5));
+	}
+	
+	@Test
+	public void testSeat() {
+		assertNull(bookingClass.makeBooking(id, "Name", "valid@email.com", "5"));
+		assertNull(bookingClass.makeBooking(id, "Name", "valid@emailcom", 2.5));
+		assertNull(bookingClass.makeBooking(id, "Name", "valid@email.com", null));
+		assertNull(bookingClass.makeBooking(id, "Name", "valid@emial.com", 0));
+		assertNull(bookingClass.makeBooking(id, "Name", "valid@emial.com", -1));
 		assertNotNull(bookingClass.makeBooking(id, "Name", "valid@email.com", 5));
 	}
 
