9dc8d321f7ed55b85ecc0f87f63edb1a5dbbdb03
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-04-10 23:45:27 +0000

    scraper much faster ⚡️

--- DayTours/scripts/arcticScraper.js
+++ DayTours/scripts/arcticScraper.js
@@ -32,7 +32,9 @@ function getCategories($) {
 }
 
 function maketour($, tourCatagory) {
+	
 	const tourTitle = $('section.hero div.orbit div.row div div h1').text();
+	
 
 	if(!tourTitle) {
 		return false;
@@ -47,7 +49,7 @@ function maketour($, tourCatagory) {
 
 	const info = {
 		title: tourTitle,
-		catagory: tourCatagory,
+		category: tourCatagory,
 		availability: $(infoBox).find('ul[data-toggle=dropdown-availability] li.icon-text p strong').text(),
 		duration: $(infoBox).find('ul[data-toggle=dropdown-duration] li.icon-text p strong').text(),
 		level: $(infoBox).find('ul[data-toggle=dropdown-level] li.icon-text p strong span').text(),
@@ -60,7 +62,7 @@ function maketour($, tourCatagory) {
 
 	const {
 		title,
-		catagory,
+		category,
 		availability= '', 
 		duration= '', 
 		level= '', 
@@ -74,7 +76,7 @@ function maketour($, tourCatagory) {
 	
 	 return {
 		title,
-		catagory,
+		category,
 		availability, 
 		duration, 
 		level, 
@@ -89,18 +91,26 @@ function maketour($, tourCatagory) {
 async function fetchCategoryTours(links) {
 	
 	const tourLinks = [];
+	const resolvedFetch = [];
 
-	// Breyta þessu í fylki af promises og nota promise.all.then... fyrir betri tima
-	for (const category of links) {
-		const response = await fetch(category.link);
-		const html = await response.text();
-		
-		const $ = cheerio.load(html);
+	let tmp;
+	let text;
+
+	await Promise.all((links).map(async (category) => {
+		tmp = await fetch(category.link);
+		text = await tmp.text();
+		resolvedFetch.push({html: text, category: category.title});
+	}));
+	
+	
+	for (const obj of resolvedFetch) {
+
+		const $ = cheerio.load(obj.html);
 		const a = $('div.row div.large-10.medium-9.medium-push-3.large-push-2.columns div.row.bottom-margin div a');
 
 		a.each((i, link) => {
 			const href = $(link).attr('href');
-			tourLinks.push({ link: href, category: category.title });
+			tourLinks.push({ link: href, category: obj.category });
 		});
 	}
 
@@ -109,22 +119,44 @@ async function fetchCategoryTours(links) {
 
 async function makeToursFromCategories(categories) {
 	const tours = [];
+	const resolvedFetch = [];
 
-	// breyta þessu í fylki af promises og nota promise.all.then... fyrir betri tima
-	for (const element of categories) {
-		const response = await fetch(element.link);
-		const html = await response.text();
-		
-		const $ = cheerio.load(html);
+	const round1 = categories.slice(0 * categories.length / 6, 1 * categories.length / 6);
+	const round2 = categories.slice(1 * categories.length / 6, 2 * categories.length / 6);
+	const round3 = categories.slice(2 * categories.length / 6, 3 * categories.length / 6);
+	const round4 = categories.slice(3 * categories.length / 6, 4 * categories.length / 6);
+	const round5 = categories.slice(4 * categories.length / 6, 5 * categories.length / 6);
+	const round6 = categories.slice(5 * categories.length / 6, 6 * categories.length / 6);
 
+	// Fleiri svona === minna load á server === minni líkur á að tapa gögnum (erum að tapa ca 15 - 25 núna all good)
+	await mapHelper(round1, resolvedFetch);
+	await mapHelper(round2, resolvedFetch);
+	await mapHelper(round3, resolvedFetch);
+	await mapHelper(round4, resolvedFetch);
+	await mapHelper(round5, resolvedFetch);
+	await mapHelper(round6, resolvedFetch);
+	
+	for (const element of resolvedFetch) {
+		const $ = cheerio.load(element.html);
 		const tour = maketour($, element.category);
 		
 		if (tour) {
 			tours.push(tour);
 		}
 	}
-	return tours;
 	
+	return tours;
+}
+
+async function mapHelper(array, returnArray) {
+	let tmp;
+	let text;
+
+	await Promise.all((array).map(async (category) => {
+		tmp = await fetch(category.link);
+		text = await tmp.text();
+		returnArray.push({html: text, category: category.category});
+	}));
 }
 
 async function main() {
@@ -135,17 +167,21 @@ async function main() {
 	
 	// sækja alla categories og linka á þeirra síðu
 	const linkCategories = getCategories($);
-	console.log('1.stage done');
+	console.log('1.stage done, reading categories');
 	
 	
 	// sækja allar ferðir af catagory síðu
 	const categories = await fetchCategoryTours(linkCategories);
-	console.log('2.stage done');
+	console.log('2.stage done, getting tours from categories');
 	
 
 	// búa til json með uppl um allar ferðir
 	const toursCategories = await makeToursFromCategories(categories);
-	console.log('3.stage done');
+	console.log('Trips to be added ',toursCategories.length);
+	
+	
+	
+	console.log('3.stage done, inserting to database');
 
 	// setja i gagnagrunn
 	const db = new sqlite3.Database('src/database/DayTours.db');
@@ -156,7 +192,7 @@ async function main() {
 		toursCategories.forEach(dayTour => {
 			const {
 				title,
-				catagory,
+				category,
 				availability, 
 				duration, 
 				level, 
@@ -169,7 +205,7 @@ async function main() {
 
 				stmt.run(
 					[title,
-					catagory,
+					category,
 					availability, 
 					duration, 
 					level, 
@@ -179,13 +215,14 @@ async function main() {
 					price,
 					description], 
 				function(err, row) {
-					console.error('row already exists');
+					if(err) {
+						console.error(err);
+					}
 				});
 		});
 	});
-	
-	db.close();
-	
+
+	console.log('database loaded and will close');
 }
 
 main();
--- DayTours/src/scripts/RunScript.java
+++ DayTours/src/scripts/RunScript.java
@@ -4,12 +4,10 @@ import java.io.*;
 
 public class RunScript {
   public static void main(String[] args) throws IOException, InterruptedException {
-      System.out.println(System.getProperty("user.dir"));
 	   ProcessBuilder pb = new ProcessBuilder("scripts/test.sh", "");
 	   pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
 	   pb.redirectError(ProcessBuilder.Redirect.INHERIT);
            Process process = pb.start();
            process.waitFor();
-           System.out.println(process.isAlive());
   }
 }
